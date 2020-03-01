ec21f7a6084036a8f63ab17c4c788b6bd8ef7b88
Alisson Zampietro <alisson.zampietro@gmail.com>
2020-03-01 23:47:54 +0000

    finish game

--- README.md
+++ README.md
@@ -4,9 +4,9 @@ Building a game with FLUTTER s2.
 
 ## Content
 
-* [Gestures](#)
-    * [Touching in the screen](lib/Game.dart#L14)
+* [Gestures](lib/Game.dart#L144)
+    * [onTap](lib/Game.dart#L145)
 * [Layout Objects](lib/modules/LayoutObjects.dart)
     * [Alignment](#)
-        * [Cross Alignment](lib/Game.dart#L32)
-        * [Main Alignment](lib/Game.dart#L51)
+        * [Cross Alignment](lib/Game.dart#L110)
+        * [Main Alignment](lib/Game.dart#L142)
--- lib/Game.dart
+++ lib/Game.dart
@@ -12,7 +12,71 @@ class _GameState extends State<Game> {
 
   final double size = 80.0;
 
+  List options = [
+      'pedra',
+      'papel',
+      'tesoura'
+  ];
+
+  var relationWinner = {
+    'pedra': {
+      "win": "tesoura",
+      "lose": "papel"
+    },
+    'papel': {
+      "win": "pedra",
+      "lose": "tesoura"
+    },
+    'tesoura': {
+      "win": "papel",
+      "lose": "pedra"
+    }
+  };
+
+  String actualImage = "padrao";
+  String message = "";
+  var textColor;
+  int scoreApp = 0;
+  int scorePlayer = 0;
+
   MaterialAccentColor actualColor;
+
+  void setAutoImage(String image) {
+    this.setState(() {
+      this.actualImage = image;
+    });
+  }
+
+  void selectOption(String player) {
+    String computer = this.options[Random().nextInt(3)];
+    String message = isWinner(computer, player);
+    this.setState((){
+      this.actualImage = computer;
+      this.message = message;
+    });
+  }
+
+  String isWinner(String computer, String player) {
+    var playerProperties;
+    this.relationWinner.forEach((key, item) {
+      if(key == player) {
+        playerProperties = item;
+      }
+    });
+
+    if(playerProperties["win"] == computer) {
+      this.textColor = Colors.green;
+      this.scorePlayer++;
+      return "You win";
+    }else if(playerProperties["lose"] == computer) {
+      this.textColor = Colors.deepOrange;
+      this.scoreApp++;
+      return "You lose";
+    }else {
+      this.textColor = Colors.black;
+      return "Draw";
+    }
+  }
 
   Text customTitle(String text) {
     return Text(
@@ -46,15 +110,25 @@ class _GameState extends State<Game> {
         crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
           Padding(
-            padding: EdgeInsets.only(top: 32, bottom: 10),
+            padding: EdgeInsets.only(top: 32, bottom: 16),
             child: customTitle('App choice: '),
           ),
-          GestureDetector(
-            onDoubleTap: changeThemeColors,
-            child: Image.asset(
-              'assets/images/padrao.png',
-              height: this.size,
-            ),
+          Image.asset("assets/images/"+this.actualImage+".png"),
+          Padding(
+              padding: EdgeInsets.all(20),
+              child: Text(
+                this.message,
+                style: TextStyle(
+                    color: this.textColor,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold
+                ),
+          )),
+          Column(
+            children: <Widget>[
+              Text("APP Score: ${this.scoreApp}"),
+              Text("Your Score: ${this.scorePlayer}"),
+            ],
           ),
           Padding(
             padding: EdgeInsets.only(top: 32, bottom: 10),
@@ -67,23 +141,31 @@ class _GameState extends State<Game> {
             * */
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: <Widget>[
-              Image.asset(
+              GestureDetector(
+                onTap: () => this.selectOption("pedra"),
+                child: Image.asset(
                   'assets/images/pedra.png',
                   height: this.size,
-
+                ),
               ),
-              Image.asset(
+              GestureDetector(
+                onTap: () => this.selectOption("papel"),
+                child: Image.asset(
                   'assets/images/papel.png',
                   height: this.size,
+                ),
               ),
-              Image.asset(
+              GestureDetector(
+                onTap: () => this.selectOption("tesoura"),
+                child: Image.asset(
                   'assets/images/tesoura.png',
                   height: this.size,
+                ),
               ),
             ],
           ),
           Padding(
-              padding: EdgeInsets.only(top: 100),
+              padding: EdgeInsets.only(top: 50),
               child: RaisedButton(
                 onPressed: this.changeThemeColors,
                 color: Colors.green,
